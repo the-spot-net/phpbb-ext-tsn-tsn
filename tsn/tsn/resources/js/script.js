c78875b8132d8916e58b5caa5048c8146f385205
neotsn neotsn <neotsn@gmail.com>
2020-01-06 10:09:39 -0600

    - Add login page, styles, element-related script - Remove login fields from myspot template

--- tsn/tsn/resources/js/script.js
+++ tsn/tsn/resources/js/script.js
@@ -1,4 +1,6 @@
 import { MDCTextField } from '@material/textfield/component';
+import { MDCCheckbox } from '@material/checkbox/component';
+import { MDCFormField } from '@material/form-field/component';
 import tsnHeader from './tsn/tsnHeader';
 
 $(document).ready(function () {
@@ -9,4 +11,25 @@ $(document).ready(function () {
     // eslint-disable-next-line no-new
     new MDCTextField(obj);
   });
+
+  // $('.mdc-switch').each(function (i, obj) {
+  //   // eslint-disable-next-line no-new
+  //   new MDCSwitch(obj);
+  // });
+
+  $('.mdc-checkbox').each(function (i, obj) {
+    // eslint-disable-next-line no-new
+    new MDCCheckbox(obj);
+    const $checkbox = $(obj);
+
+    const $formField = $checkbox.parents('.mdc-form-field');
+    if ($formField) {
+      // eslint-disable-next-line no-new
+      new MDCFormField($formField[0]);
+    }
+  });
+
+  // TODO Hook up the Login form in its own class so the checkboxes/switches can be turned into form fields
+  // const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
+  // formField.input = checkbox;
 });

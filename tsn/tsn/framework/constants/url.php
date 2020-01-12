e1b2bee64b8e5eb544f4095e44ac763af5e5aa42
neotsn neotsn <neotsn@gmail.com>
2020-01-11 18:14:59 -0600

    Cleanup URL class to remove everything but constants, none of it is supposed to be used.

--- tsn/tsn/framework/constants/url.php
+++ tsn/tsn/framework/constants/url.php
@@ -15,60 +15,13 @@ namespace tsn\tsn\framework\constants;
  */
 class url
 {
-    // URI Route Directories
-    const DIR_TSN = 'tsn/';
-    const DIR_AJAX = self::DIR_TSN . 'ajax/';
-
     // URI Base Routes
     const ROUTE_INDEX = 'tsn_tsn_index';
     const ROUTE_FORUM = 'tsn_tsn_forum';
-//    const ROUTE_GROUP = self::DIR_TSN . 'group';
+    const ROUTE_GROUP = 'tsn_tsn_group';
     const ROUTE_LOGIN = 'tsn_tsn_login';
     const ROUTE_MEMBER = 'tsn_tsn_member';
-    const ROUTE_TOPIC = 'tsn_tsn_topic';
     const ROUTE_POST = 'tsn_tsn_post';
+    const ROUTE_TOPIC = 'tsn_tsn_topic';
     const ROUTE_USER = 'tsn_tsn_user';
-
-    // AJAX Slugs for use in switch & routes
-    const SLUG_SPECIAL_REPORT = 'special-report';
-
-    // URI AJAX Routes
-    const ROUTE_AJAX_SPECIAL_REPORT = self::DIR_AJAX . self::SLUG_SPECIAL_REPORT;
-
-    /**
-     * @param       $routeUriConstant
-     * @param array $appendedIds Ordered array of IDs to append to the URL (.../list, [forumId, topicId])
-     *                           //param array $requestParams Field-value pairs to pass to url;::makeUrlParams()
-     *
-     * @return string
-     * @uses \tsn\tsn\controller\url::makeUrlParams()
-     */
-//    public static function make($routeUriConstant, $appendedIds = [], $requestParams = [])
-    public static function make($routeUriConstant, $appendedIds = [])
-    {
-        return implode(array_filter([
-                $routeUriConstant,
-                ($appendedIds) ? '/' . implode('/', $appendedIds) : '',
-                // ($requestParams) ? self::makeUrlParams($requestParams) : '',
-            ]
-        ));
-    }
-
-//    /**
-//     * @param array $params  Field-value pairs to generate the query string
-//     * @param null  $urlHash URL Hash to jump to an anchor or trigger something to open
-//     *
-//     * @return string
-//     */
-//    public static function makeUrlParams(array $params = [], $urlHash = null)
-//    {
-//        $params = array_filter($params);
-//        $urlHash = trim(ltrim($urlHash, '#'));
-//
-//        $queryString = '';
-//        $queryString .= ($params) ? '?' . http_build_query($params) : '';
-//        $queryString .= ($urlHash) ? '#' . $urlHash : '';
-//
-//        return $queryString;
-//    }
 }
